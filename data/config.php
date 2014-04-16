a536891e01be2a6b7fb33e127a52be6e0cd0353e
bipeis <bipeis@gmail.com>
2014-04-16 12:09:32 +0200

    Deletes y html5

--- data/config.php
+++ data/config.php
@@ -1,19 +1,5 @@
 <?php
 
-  //BBDD
-/*
-  $servidor = "localhost";
-  $usuario = "root";
-  $contrasena = "daw";
-  $bbdd = "bdpvm";
-*/
-/*
-  $servidor = "localhost";
-  $usuario = "pvmuser";
-  $contrasena = "pvmuser";
-  $bbdd = "pvnbd";
-*/
-
   $servidor = "tutextilhogar.com";
   $usuario = "tutextil_pvm";
   $contrasena = "bil3600p";
